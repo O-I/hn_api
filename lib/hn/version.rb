f1f800c6a14561f046a53fd7c41e5ab33e4ae54f
Rahul Horé <hore.rahul@gmail.com>
2014-10-07 16:03:56 -0400

    Adds vanilla version.rb

---
+++ lib/hn/version.rb
@@ -0,0 +1,14 @@
+module HN
+  class Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 1
+    PRE   = nil
+
+    class << self
+      def to_s
+        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+      end
+    end
+  end
+end
