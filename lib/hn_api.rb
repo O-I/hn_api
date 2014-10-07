6167dec2a3508fbaccd92a895d5a79dab31aa1a2
Rahul Horé <hore.rahul@gmail.com>
2014-10-07 14:31:49 -0400

    Adds vanilla hn_api.rb

---
+++ lib/hn_api.rb
@@ -0,0 +1,17 @@
+module HN
+  class << self
+
+    def new
+      @client ||= HN::Client.new
+    end
+
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
+
+    def respond_to?(method, include_private = false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
+  end
+end
