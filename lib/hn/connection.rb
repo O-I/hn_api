766673a31b3687bb253ad850e6af9bb0b7bbf3a1
Rahul Horé <hore.rahul@gmail.com>
2014-10-07 14:55:03 -0400

    Adds vanilla connection.rb

---
+++ lib/hn/connection.rb
@@ -0,0 +1,22 @@
+require 'faraday_middleware'
+
+module HN
+  module Connection
+
+    private
+
+    def connection
+      options = {
+        ssl: { verify: false },
+        url: base_url
+      }
+
+    Faraday.new(options) do |connection|
+      connection.use Faraday::Request::UrlEncoded
+      connection.use Faraday::Response::RaiseError
+      connection.use Faraday::Response::Mashify
+      connection.use Faraday::Response::ParseJson
+      connection.adapter(Faraday.default_adapter)
+    end
+  end
+end
